94f7dda7bdf6d3a0e8a277e505485797a5873fd6
Zhong Zhu <zhongzhu0421@gmail.com>
2016-06-30 00:47:04 -0400

    add click-and-disable directive and loading spinner

--- enron_oaqa/static/enron/app.js
+++ enron_oaqa/static/enron/app.js
@@ -1,153 +1,219 @@
+
 (function() {
-	var app = angular.module("enron", []);
+  var app = angular.module("enron", []);
 
-	app.service('DataService', [
-			'$http',
-			function($http) {
-				this.questions = [];
+  app.service('DataService', [
+    '$http',
+    function($http) {
+      this.questions = [];
 
-				this.getHistoryQuestions = function() {
-					var questions = this.questions;
-					return $http.get('/enron/api/history_questions').then(function(data) {
-						// console.log("data retrieved: " + data);
-						for (var i = 0; i < data.data.length; i++) {
-							questions.push(data.data[i]);
-						}
-						return questions;
-					});
-				};
+      this.getHistoryQuestions = function() {
+        var questions = this.questions;
+        return $http.get('/enron/api/history_questions').then(function(data) {
+          // console.log("data retrieved: " + data);
+          for (var i = 0; i < data.data.length; i++) {
+            questions.push(data.data[i]);
+          }
+          return questions;
+        });
+      };
 
-				this.addHistoryQuestion = function(newQuestion) {
-					var req = { method : 'POST', url : '/enron/api/history_questions/',
-						headers : { 'Content-Type' : 'application/json' }, data : newQuestion };
+      this.addHistoryQuestion = function(newQuestion) {
+        var req = {
+          method: 'POST',
+          url: '/enron/api/history_questions/',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: newQuestion
+        };
 
-					var questions = this.questions;
-					$http(req).then(function() {
-						console.log("Question inserted!");
-						questions.unshift(newQuestion);
-					});
-				};
-			} ]);
+        var questions = this.questions;
+        $http(req).then(function() {
+          console.log("Question inserted!");
+          questions.unshift(newQuestion);
+        });
+      };
+    }
+  ]);
 
-	app.service('answerService', function() {
-		this.answers = [];
+  app.service('answerService', function() {
+    this.answers = [];
 
-		this.setAnswers = function(newObj) {
-			for (var i = 0; i < newObj.length; i++) {
-				this.answers[i] = newObj[i];
-			}
-		};
+    this.setAnswers = function(newObj) {
+      for (var i = 0; i < newObj.length; i++) {
+        this.answers[i] = newObj[i];
+      }
+    };
 
-		this.getAnswers = function() {
-			return this.answers;
-		};
-	});
+    this.getAnswers = function() {
+      return this.answers;
+    };
+  });
 
-	app.service('questionService', ['$http', 'answerService', 'DataService', function($http, answerService, DataService) {
-		this.submitQuestion = function(questionText) {
-			var liveqaData = { 'qid' : '20130828153959AAtXAEs', 'title' : questionText,
-					'body' : '', 'category' : '', };
-				
-				// Send request from client side.
-				// var req = { method : 'POST', url :
-				// 'http://gold.lti.cs.cmu.edu:18072/liveqa',
-				// headers : { 'Content-type' : 'application/json', 'Accept' :
-				// 'application/json' },
-				// data : JSON.stringify(liveqaData) };
+  app.service('questionService', [
+    '$http',
+    'answerService',
+    'DataService',
+    function($http, answerService, DataService) {
+      this.submitQuestion = function(questionText) {
+        var liveqaData = {
+          'qid': '20130828153959AAtXAEs',
+          'title': questionText,
+          'body': '',
+          'category': '',
+        };
 
-				var req = { method : 'POST', url : '/enron/api/get_answers/',
-					headers : { 'Content-type' : 'application/json', 'Accept' : 'application/json' },
-					data : JSON.stringify(liveqaData) };
+        // Send request from client side.
+        // var req = { method : 'POST', url :
+        // 'http://gold.lti.cs.cmu.edu:18072/liveqa',
+        // headers : { 'Content-type' : 'application/json', 'Accept' :
+        // 'application/json' },
+        // data : JSON.stringify(liveqaData) };
 
-				$http(req).success(
-						function(data, status, headers, config) {
-							console.log("Question submitted! Result is:");
-							console.log(data);
-							console.log(status);
+        var req = {
+          method: 'POST',
+          url: '/enron/api/get_answers/',
+          headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+          },
+          data: JSON.stringify(liveqaData)
+        };
 
-							var answers = [];
-							var returnedAnswers = data.answers.candidates;
-							for (var i = 0; i < returnedAnswers.length; i++) {
-								answers.push({ 'id' : i, 'source' : returnedAnswers[i].url,
-									'score' : returnedAnswers[i].score, 'body' : returnedAnswers[i].bestAnswer });
-							}
-							console.log("answers: " + answers);
-							answerService.setAnswers(answers);
-							var newQuestion = { question : questionText };
-							DataService.addHistoryQuestion(newQuestion);
-						}).error(function(data, status, headers, config) {
-					console.log("Question submission failed!");
-					console.log(data);
-					console.log(status);
-				});
-				// Testing code.
-				// $http.get("http://jsonplaceholder.typicode.com/posts").success(function(data)
-				// {
-				// console.log("return " + data.length + " answers with the first
-				// being " + data[0].id);
-				// answerService.setAnswers(data);
-				// });
-		};
-	}]);
+        return $http(req).then(function successCallback(response) {
+          console.log("Question submitted! Result is:");
+          console.log(response.data);
+          console.log(response.status);
 
-	app.config(function($interpolateProvider) {
-		$interpolateProvider.startSymbol('[[[');
-		$interpolateProvider.endSymbol(']]]');
-	});
+          var answers = [];
+          var returnedAnswers = response.data.answers.candidates;
+          for (var i = 0; i < returnedAnswers.length; i++) {
+            answers.push({
+              'id': i,
+              'source': returnedAnswers[i].url,
+              'score': returnedAnswers[i].score,
+              'body': returnedAnswers[i].bestAnswer
+            });
+          }
+          console.log("answers: " + answers);
+          answerService.setAnswers(answers);
+          var newQuestion = {
+            question: questionText
+          };
+          DataService.addHistoryQuestion(newQuestion);
+        }, function errorCallback(response) {
+          console.log("Question submission failed!");
+          console.log(response.data);
+          console.log(response.status);
+        });
+        // Testing code.
+        // $http.get("http://jsonplaceholder.typicode.com/posts").success(function(data)
+        // {
+        // console.log("return " + data.length + " answers with the first
+        // being " + data[0].id);
+        // answerService.setAnswers(data);
+        // });
+      };
+    }
+  ]);
 
-	console.log("app.js is loaded.");
+  app.config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[[');
+    $interpolateProvider.endSymbol(']]]');
+  });
 
-	app.controller("Question", [
-			"DataService",
-			"questionService",
-			"answerService",
-			function(DataService, questionService, answerService) {
-				this.questionText = ""; // initial value;
+  console.log("app.js is loaded.");
 
-				this.submitQuestion = function() {
-					console.log("QuestionText: " + this.questionText);
-					
-					questionService.submitQuestion(this.questionText);
+  app.controller("Question", ["DataService", "questionService", "answerService",
+    function(DataService, questionService, answerService) {
+      this.questionText = ""; // initial value;
 
-					// Zhong: keep the question text
-					//this.questionText = "";
-				};
-			} ]);
+      this.submitQuestion = function() {
+        console.log("QuestionText: " + this.questionText);
 
-	app.controller("Answer", [ "$scope", "answerService", function($scope, answerService) {
-		this.answers = answerService.getAnswers();
-		this.thumbsUp = new Array(6);
-		this.thumbsDown = new Array(6);
-		for (var i = 0; i < 6; ++i) {
-			this.thumbsUp[i] = false;
-			this.thumbsDown[i] = false;
-		}
+        return questionService.submitQuestion(this.questionText);
 
-		this.thumbsUp = function(answerID) {
-			console.log("Thumbs up for answer {" + answerID + "}.")
-			this.thumbsUp[answerID] = !this.thumbsUp[answerID];
-			this.thumbsDown[answerID] = false;
-		};
+        // Zhong: keep the question text
+        // this.questionText = "";
+      };
+    }
+  ]);
 
-		this.thumbsDown = function(answerID) {
-			console.log("Thumbs down for answer {" + answerID + "}.")
-			this.thumbsDown[answerID] = !this.thumbsDown[answerID];
-			this.thumbsUp[answerID] = false;
-		};
+  app.controller("Answer", ["$scope", "answerService", function($scope, answerService) {
+    this.answers = answerService.getAnswers();
+    this.thumbsUp = new Array(6);
+    this.thumbsDown = new Array(6);
+    for (var i = 0; i < 6; ++i) {
+      this.thumbsUp[i] = false;
+      this.thumbsDown[i] = false;
+    }
 
-	} ]);
+    this.thumbsUp = function(answerID) {
+      console.log("Thumbs up for answer {" + answerID + "}.")
+      this.thumbsUp[answerID] = !this.thumbsUp[answerID];
+      this.thumbsDown[answerID] = false;
+    };
 
-	app.controller("History", [ "$scope", "DataService", "questionService", function($scope, DataService, questionService) {
-		DataService.getHistoryQuestions().then(function(historyQuestions) {
-			$scope.historyQuestions = historyQuestions;
-			// console.log("History questions: " + $scope.historyQuestions);
-		});
-		
-//		$scope.$watch(function() { return Service.getNumber(); }, function(value) { $scope.number = value; });
+    this.thumbsDown = function(answerID) {
+      console.log("Thumbs down for answer {" + answerID + "}.")
+      this.thumbsDown[answerID] = !this.thumbsDown[answerID];
+      this.thumbsUp[answerID] = false;
+    };
 
-		this.askQuestion = function(s) {
-			questionService.submitQuestion(s);
-		};
-		
-	} ]);
+  }]);
+
+  app.controller("History", ["$scope", "DataService", "questionService",
+    function($scope, DataService, questionService) {
+      DataService.getHistoryQuestions().then(function(historyQuestions) {
+        $scope.historyQuestions = historyQuestions;
+        // console.log("History questions: " + $scope.historyQuestions);
+      });
+
+      // $scope.$watch(function() { return Service.getNumber(); },
+      // function(value) { $scope.number = value; });
+
+      this.askQuestion = function(s) {
+        questionService.submitQuestion(s);
+      };
+
+    }
+  ]);
+
+  app.directive('loading', ['$http', function($http) {
+    return {
+      restrict: 'A',
+      link: function(scope, elm, attrs) {
+        scope.isLoading = function() {
+          return $http.pendingRequests.length > 0;
+        };
+
+        scope.$watch(scope.isLoading, function(v) {
+          if (v) {
+            elm.show();
+          } else {
+            elm.hide();
+          }
+        });
+      }
+    };
+  }]);
+
+  app.directive('clickAndDisable', function() {
+    return {
+      scope: {
+        clickAndDisable: '&'
+      },
+      link: function(scope, iElement, iAttrs) {
+        iElement.bind('click', function() {
+          iElement.prop('disabled',true);
+          console.log("scope.clickAndDisble is " + scope.clickAndDisable);
+          scope.clickAndDisable().finally(function() {
+            iElement.prop('disabled',false);
+          })
+        });
+      }
+    };
+  });
+
 })();
