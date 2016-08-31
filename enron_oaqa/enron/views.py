aea0c6533f4533c5d9820745018c5d8eeb81440b
salmedina <salbed.medina@gmail.com>
2016-08-31 00:47:28 -0400

    Added Use LiveQA and use Enron corpus logic, there is still a bug while toggling from liveqa to enron and back, that keeps the liveqa answers appearing even if it was not selected

--- enron_oaqa/enron/views.py
+++ enron_oaqa/enron/views.py
@@ -54,49 +54,65 @@ def history_questions(request):
 def get_answers(request):
     useEnron = request.data['options']['useEnron']
     useLiveQA = request.data['options']['useLiveQA']
-    # Requesting info from LIVE-QA
-    url = "http://gold.lti.cs.cmu.edu:18072/liveqa"
-    data = {"qid":"20130828153959AAtXAEs",
-            "title":request.data['title'],
-            "body":"",
-            "category":""}
-    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-    r = requests.post(url, data=json.dumps(data), headers=headers)
-    result = json.loads("".join(r))
-    for answer in result['candidates']:
-        answer['shortUrl'] = re.search(r"https?://([^/]+)/", answer['url']).group(1)
 
-    # Requesting info from Enron Solr index
-    core_url = 'http://metal.lti.cs.cmu.edu:7574/solr/enron_shard1_replica1'
-    core = solr.SolrConnection(core_url)
-    solr_query = '{} & fl=*,score'.format(request.data['title'])
-    solr_res = core.query(solr_query)
- 
- 
-    for solr_result in solr_res.results[:10]:
-        tmp_formatted_input = {}
-        tmp_formatted_input['url'] = '#'
-        tmp_formatted_input['shortUrl'] = 'Enron Corpus - {}'.format(solr_result['file'][0])
-        tmp_formatted_input['score'] = solr_result['score']
-        tmp_formatted_input['bestAnswer'] = solr_result['body'][0].replace('\n','<br/>')
-         
-        result['candidates'].append(tmp_formatted_input)
+    # Requesting info from LIVE-QA
+    if useLiveQA:
+        url = "http://gold.lti.cs.cmu.edu:18072/liveqa"
+        data = {"qid":"20130828153959AAtXAEs",
+                "title":request.data['title'],
+                "body":"",
+                "category":""}
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        result = json.loads("".join(r))
+        for answer in result['candidates']:
+            answer['shortUrl'] = re.search(r"https?://([^/]+)/", answer['url']).group(1)
+
+    if useEnron:
+        if not useLiveQA:
+            # Create empty answer template
+            result = {}
+            result['answerResource'] = 'Enron'
+            result['answerText'] = ''
+            result['candidates'] = []
+
+        # Requesting info from Enron Solr index
+        core_url = 'http://metal.lti.cs.cmu.edu:7574/solr/enron_shard1_replica1'
+        core = solr.SolrConnection(core_url)
+        solr_query = '{} & fl=*,score'.format(request.data['title'])
+        solr_res = core.query(solr_query)
+
+
+        for solr_result in solr_res.results[:20]:
+            tmp_formatted_input = {}
+            tmp_formatted_input['url'] = '#'
+            tmp_formatted_input['shortUrl'] = 'Enron Corpus - {}'.format(solr_result['file'][0])
+            tmp_formatted_input['score'] = solr_result['score']
+            tmp_formatted_input['bestAnswer'] = solr_result['body'][0].replace('\n','<br/>')
+
+            result['candidates'].append(tmp_formatted_input)
 
     result['candidates'] = sorted(result['candidates'], key=lambda k: k['score']) 
 
     # Highlighting the results
+    # Consider only nouns, verbs, adjectives and adverbs
     valid_pos_tags = ['FW','JJ','JJR','JJS','NN','NNS','NNP','NNPS', 'RB', 'RBR', 'RBS', 'VB','VBD','VBG','VBN','VBZ','']
+    # Remove the stopwords from the english dictionary
     stop = set(stopwords.words('english'))
+    # Filter out the terms to highlight
     nltk_tagger = NLTKTagger()
     query_blob = TextBlob(request.data['title'], pos_tagger=nltk_tagger)
     highlight_terms = []
     for word, word_pos in query_blob.pos_tags:
         if word_pos in valid_pos_tags and word not in stop:
             highlight_terms.append(str(word))
-     
+
+    # Tag them with HTML spans
     for term in highlight_terms:
         highlight_regex = re.compile(term, re.IGNORECASE)
         for i in range(len(result['candidates'])):
             result['candidates'][i]['bestAnswer'] = highlight_regex.sub('<span class="relevantEntity">'+term+'</span>', result['candidates'][i]['bestAnswer'])
+
+    result['candidates'] = result['candidates'][:20]
 
     return Response({'answers': result})
